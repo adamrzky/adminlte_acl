b9692f8f740522bfe739e8117c2f137daff6895c
zainunamal <zainun.amal@gmail.com>
2022-12-26 11:11:06 +0700

    base api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    // public routes
+    Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
+    Route::post('/register', 'Auth\ApiAuthController@register')->name('register.api');
+    // Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
+});
+
+Route::middleware('auth:api')->group(function () {
+    // our routes to be protected will go in here
+    Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
 });
